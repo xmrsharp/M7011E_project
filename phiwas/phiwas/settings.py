1c194dd3138cf789ae0f2fbb2e1ecb3442bc4930
david <davidwass90@gmail.com>
2021-12-12 17:41:59 +0100

    fixed somq sql shit

--- phiwas/phiwas/settings.py
+++ phiwas/phiwas/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'phiwas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'm7011e',
+        'USER': 'admin',
+        'PASSWORD': 'adminpw',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
