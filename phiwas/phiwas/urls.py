bba92ebaa80048434778781d4b570b73b03746ea
david <davidwass90@gmail.com>
2022-01-12 22:34:01 +0100

    Register/login/logout/profile page added
    Doesnt work writing to database for some reason weird

--- phiwas/phiwas/urls.py
+++ phiwas/phiwas/urls.py
@@ -15,8 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from polls.views import ResetPasswordView
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+from polls.views import ChangePasswordView
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-]
+    path('home/', include('users.urls')),
+    path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='polls/password_reset_complete.html'),
+         name='password_reset_complete'),
+    path('password-change/', ChangePasswordView.as_view(), name='password_change'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- phiwas/polls/models.py
+++ phiwas/polls/models.py
@@ -1,6 +1,31 @@
 from datetime import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from PIL import Image
+
+
+# Extending User Model Using a One-To-One Link
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    avatar = models.ImageField(
+        default='default.jpg', upload_to='profile_images')
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+        # resizing images
+
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 100 or img.width > 100:
+            new_img = (100, 100)
+            img.thumbnail(new_img)
+            img.save(self.avatar.path)
 
 
 class Question(models.Model):
@@ -13,6 +38,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def delete_q(self):
+        self.question_text = ""
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -21,3 +49,7 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+class ProSumer(models.Model):
+    blackout = models.BooleanField(User)
